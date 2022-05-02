d954ca19715c2b114c8ebf22a0ebb22efc1ed8d3
Janisa Sri S <jani2995@global.chainsys.com>
2022-05-02 10:57:33 +0530

    first commit

--- project-eleven/src/app/app.component.ts
+++ project-eleven/src/app/app.component.ts
@@ -4,7 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
-  title = 'project-eleven';
+  // title = 'project-eleven';
+  // showLifeCycle: boolean = false;
+  // value: string ='jeni';
+
+constructor(){
+   
 }
+// private _value: string = "jeni";
+
+//   get value(): string {
+//     return this._value;
+//   }
+//   set value(val: string) {
+//     //do some extra work here
+//     this._value = val;
+//   }
+
+// onShowOrHide(): void {
+//   this.showLifeCycle = !this.showLifeCycle;
+// }
+  
+}
+
--- project-eleven/src/app/app.module.ts
+++ project-eleven/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LifeCycleComponent } from './life-cycle/life-cycle.component';
+import { LifeCycleChildComponent } from './life-cycle-child/life-cycle-child.component';
+import { CounterAppComponent } from './counter-app/counter-app.component';
+import { CounterDisplayComponent } from './counter-display/counter-display.component';
+import { HitButtonComponent } from './hit-button/hit-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LifeCycleComponent,
+    LifeCycleChildComponent,
+    CounterAppComponent,
+    CounterDisplayComponent,
+    HitButtonComponent
   ],
   imports: [
     BrowserModule,
